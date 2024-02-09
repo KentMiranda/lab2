38d913de4a5582657e53eb1a25da65973773a1f5
Kenett Fernandez <kmfernandez@student.apc.edu.ph>
2024-02-10 07:36:58 +0800

    updated

---
+++ Week5/my_file.php
@@ -0,0 +1,4 @@
+<?php
+echo '<hr style="width:200px" class="w3-opacity">';
+echo '<p>Some text about me. Some text about me. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>';
+?>
