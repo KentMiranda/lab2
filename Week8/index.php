aa40490cd8715a1ffc90bdf60faf2f785f1ee88f
Kenett Fernandez <kmfernandez@student.apc.edu.ph>
2024-02-10 23:35:06 +0800

    updated Week 8

--- Week8/index.php
+++ Week8/index.php
@@ -60,10 +60,13 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Times New Roman", serif}
   </header>
 
   <div class="w3-content w3-justify w3-text-grey w3-padding-64" id="about">
-    <h2 class="w3-text-light-grey">Kent Miranda</h2>
-      <hr style="width:200px" class="w3-opacity">
-      <p>Some text about me. Some text about me. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    <h3 class="w3-padding-16 w3-text-light-grey">My Skills</h3>
+    
+  <?php  
+    echo'Kent Miranda';  
+    echo'Some text about me. Some text about me. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+  ?> 
+    
+      <h3 class="w3-padding-16 w3-text-light-grey">My Skills</h3>
     <p class="w3-wide">SQL</p>
     <div class="w3-white">
       <div class="w3-dark-grey" style="height:28px;width:95%"></div>
